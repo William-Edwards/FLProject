54f2c8dd0b4400569fb5ee37c43c3ff5db2d03bf
William-Edwards <67965211+William-Edwards@users.noreply.github.com>
2023-05-10 17:24:04 +0100

    setting up maven

---
+++ flooringmastery/src/main/java/com/flooringmastery/App.java
@@ -0,0 +1,13 @@
+package com.flooringmastery;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
